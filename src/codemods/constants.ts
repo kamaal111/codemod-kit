fafd63d863689402fe13236e7be608c4417633a4
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-25 21:53:04 +0200

    Adding more extension mappings

--- src/codemods/constants.ts
+++ src/codemods/constants.ts
@@ -1,5 +1,16 @@
 import { Lang } from '@ast-grep/napi';
 
-export const LANG_TO_EXTENSIONS_MAPPING: Partial<Record<string, Set<string>>> = {
-  [Lang.TypeScript.toLowerCase()]: new Set(['.ts', '.tsx', '.js', '.jsx', '.cjs', '.mjs', '.mts']),
-};
+const JAVASCRIPT_EXTENSIONS = ['.js', '.cjs', '.mjs'];
+const TYPESCRIPT_EXTENSIONS = JAVASCRIPT_EXTENSIONS.concat(['.ts', '.mts']);
+const JSX_EXTENSIONS = ['.jsx'];
+
+export const LANG_TO_EXTENSIONS_MAPPING: Record<string, Set<string>> = Object.fromEntries(
+  Object.entries({
+    [Lang.TypeScript]: TYPESCRIPT_EXTENSIONS,
+    ts: TYPESCRIPT_EXTENSIONS,
+    [Lang.Tsx]: JSX_EXTENSIONS.concat(['.tsx']),
+    jsx: JSX_EXTENSIONS,
+    [Lang.JavaScript]: JAVASCRIPT_EXTENSIONS,
+    js: JAVASCRIPT_EXTENSIONS,
+  }).map(([key, value]) => [key.toLowerCase(), new Set(value)]),
+);
